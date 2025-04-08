6c0df06d3112c1e4c8dee2fac1367a0dc9257498
user <tmdrnf35@g.shingu.ac.kr>
2025-04-08 11:37:46 +0900

    2번째 3번째 큰 수 구하기

--- p02Collatz.py
+++ p02Collatz.py
@@ -57,14 +57,4 @@ print(f'{secondvalue=}, {secondvaluen=}')
 print(f'{thirdvalue=}, {thirdvaluen=}')
 
 
-# def collatz_steps(n):
-#     steps = 0
-#     while n!= 1:
-#         if n % 2 == 1:
-#             n = 3 * n + 1
-#     else:
-#         n = n // 2
-#     steps += 1
-#     return steps
-# max_steps = 0
-# for i in range(1, 101):
+
