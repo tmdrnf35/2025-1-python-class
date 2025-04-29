17ba10926fc0099288b238a2888d4642e5d0eeb2
user <tmdrnf35@g.shingu.ac.kr>
2025-04-29 12:28:26 +0900

    객체지향실습1

--- 06soccerclass.py
+++ 06soccerclass.py
@@ -14,7 +14,8 @@ class SoccerPlayer(object):
     def change_back_number(self, new_number):
         print(f'선수 번호 교체: {self.back_number} => {new_number}')
 
-
+    def __str__(self):
+        return f"저의 이름은{self.name}, 위치는 {self.position}, 번호는 {self.back_number} 입니다. 화이팅"
 # SoccerPlayer 클래스가 jh 오브젝트를 생성함
 # 클래스의 생성함수 constructor가 실행됨
 jh = SoccerPlayer("김종현", 'MF', 10)
@@ -31,4 +32,31 @@ jh.change_back_number(5)
 # 교체된 백넘버 확인
 print(f'{jh.back_number=}')
 
+names = ["Messi", "Ramos", "Ronlado", "park", "son"]
+positions = ["MF", "DF", "CF", "WF", "GK"]
+numbers = [10, 9, 8, 7 , 1]
 
+players = [[name, position, number] for name, position, number in zip(names, positions, numbers)]
+print(players)
+print(players[0])
+
+# 첫번째 선수로 messi 생성
+messi = SoccerPlayer(players[0][0], players[0][1], players[0][2])
+print(messi.name)
+
+# 선수들의 리스트를 생성
+player_objects = [SoccerPlayer(name, position, number) for name, position, number in zip(names, positions, numbers)]
+print(player_objects[0])
+
+# 첫번째 선수의 이름
+print(player_objects[0].name)
+
+# 모든 선수의 이름, 포지션, 번호 출력
+for player in player_objects:
+    print(player)
+
+#print(player_objects[0])
+#print(player_objects[1])
+#print(player_objects[2])
+#print(player_objects[3])
+#print(player_objects[4])
